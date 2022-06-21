d59276ea8ed6c99d66881163a84323aa8efe65ae
brenxm <brymina18@yahoo.com>
2022-06-21 11:16:18 -0500

    Add functionality to update grids through slider input and text input

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ let onMouseDown = false;
 const sliderContainer = document.getElementById('slider-container');
 const pixelButton = document.querySelector('#pixel');
 const pixelSlider = document.querySelector('#pxl-slider');
+const textPxl = document.querySelector('#current-pxl');
+let currentPxl = 64;
 
-//generateGrids(16);
+generateGrids(currentPxl);
 
+
+//generate grids and also will delete pre-exising grids if there is any
 function generateGrids(count){
+    gridContainer.replaceChildren();
     for(let i = 0; i < count * count; i++){
         let temp = document.createElement('div');
         temp.setAttribute('id','grid');
@@ -16,6 +21,12 @@ function generateGrids(count){
     }
 
     updateGridsSize(grids);
+
+    grids.forEach(
+        (x) => {
+            x.addEventListener('mouseover', paint)
+        }
+    )
 };
 
 
@@ -28,7 +39,6 @@ function updateGridsSize(nodeList){
    
 };
 
-//update grid count
 
 
 //media queries updating the size each grid 
@@ -45,13 +55,7 @@ function paint(event){
 }
 
 //initializing event listeners
-/*
-grids.forEach(
-    (x) => {
-        x.addEventListener('mouseover', paint)
-    }
-)
-*/
+
 
 pixelButton.addEventListener('click',()=>{sliderContainer.style.display == 'none' ? 
     sliderContainer.style.display = 'block' : sliderContainer.style.display = 'none'; 
@@ -68,7 +72,68 @@ document.body.onmouseup = () => {
 
 
 //pixel input value change
-pixelSlider.addEventListener('change',()=>{
-    console.log(`changing! and the current value is ${event.target.value}` );
+pixelSlider.addEventListener('change',(event)=>{
+    changePxl(null, event.target.value);
 })
 
+//current pxl text updating fn
+//input a number between 1 to 100 (inclusive)
+    //alert if out of number
+    //acceptable format
+        //number <100>
+        //number x number <100x100> or <100 x 100> (or with space)
+            //first number and second number must be the same, otherwise it alert an error
+
+
+//function referenced to slider and text input upon submit
+function changePxl(event, sliderInput) {
+    if(event != null){
+        event.preventDefault();
+
+        if(/^\d+$/.test(textPxl.value)){
+            if(textPxl.value > 0 && textPxl.value < 101){
+                pixelSlider.value = textPxl.value;
+                textPxl.blur();
+                generateGrids(textPxl.value);
+                updatePxlText(textPxl.value);
+            }
+
+            else{
+                alert('out of accepted value')
+                console.log(textPxl.value);
+            }
+        }
+
+        else if(/^\d+\s?[xX]\s?\d+$/.test(textPxl.value)){
+            const match = textPxl.value.match(/(\d+)\s?[xX]\s?(\d+)/);
+            if(match[1] === match[2]){
+                if(match[1] > 0 && match[1] < 101){
+                    pixelSlider.value = match[1];
+                    textPxl.blur();
+                    generateGrids(match[1]);
+                    updatePxlText(match[1]);
+                }
+                else{
+                    alert('out of accepted value');
+                }
+            }
+            
+            else{
+                alert('invalid pixel input');
+            }
+        }
+    }
+
+    else if(sliderInput != undefined){
+        updatePxlText(sliderInput);
+        generateGrids(sliderInput);
+    }
+
+    function updatePxlText(pxl){
+        textPxl.value = `${pxl}x${pxl}`;
+    }
+}
+
+document.getElementById('pxl-form').addEventListener('submit',(event)=>{
+    changePxl(event, null);
+});
