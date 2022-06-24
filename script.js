572bed9933536d56c4f0adad0600484ac32222dd
brenxm <brymina18@yahoo.com>
2022-06-23 20:59:22 -0500

    Progress painting function and erasing function

--- script.js
+++ script.js
@@ -12,23 +12,14 @@ const opcSlider = document.querySelector('#opc-slider');
 const eraserButton = document.querySelector('#eraser');
 const colorButton = document.querySelector('#color');
 const rainbowButton = document.querySelector('#rainbow');
-let activeBrush;
-let r;
-let g;
-let b;
-let o;
 let currentPxl = 64;
 
 //on start up
-//generateGrids(currentPxl);
+generateGrids(currentPxl);
 eventListenersFunction();
 document.getElementById('color-indicator').style.display = 'block';
-activeBrush = 'rgba(255, 0, 0, 1);';
-r = 0; 
-g = 0;
-b = 0;
-o = 1;
-
+const activeBrush = {r: 0, g: 0, b: 0, a: opcSlider.value};
+const colorBrush = {r: 0, g: 0, b: 0, a: 0.5};
 
 //generate grids and also will delete pre-exising grids if there is any
 function generateGrids(count) {
@@ -54,7 +45,7 @@ function generateGrids(count) {
 function updateGridsSize(nodeList) {
     const containerSize = gridContainer.clientHeight / Math.sqrt(nodeList.length);
     nodeList.forEach(element => {
-        element.setAttribute('style', `height: ${containerSize}px; width: ${containerSize}px; box-sizing: border-box;`)
+        element.setAttribute('style', `height: ${containerSize}px; width: ${containerSize}px; box-sizing: border-box; background-color: rgb(255, 255, 255);`)
     });
 
 };
@@ -62,17 +53,25 @@ function updateGridsSize(nodeList) {
 
 //function to draw colors
 function paint(event) {
-    const currentRGB = window.getComputedStyle(event.target).getPropertyValue('background-color');
-    const rgb = currentRGB.match(/rgba[(](\d+), (\d+), (\d+), \d+[)]/);
-    const finalRGB = `rgb(${(rgb[1] / o) + (r / o)})`
+    const currentRGB = event.target.style.getPropertyValue('background-color');
+    
+    const rgb = currentRGB.match(/rgb[(](\d+), (\d+), (\d+)[)]/);
+    
 
-    event.target.setAttribute('style', `${event.target.getAttribute('style')} background-color: ${activeBrush};`);
+    /*
+    event.target.setAttribute('style', `${event.target.getAttribute('style')} background-color: rgb(${(rgb[1] * opcSlider.value) + (r * opcSlider.value)}, ${(rgb[2] * opcSlider.value) + (g * opcSlider.value)}, ${(rgb[3] * opcSlider.value) + (b * opcSlider.value)});`);
+    */
 
-    console.log(event.target.getAttribute('style'));
+    event.target.setAttribute('style', `${event.target.getAttribute('style')} background-color: rgb(${(rgb[1] - (rgb[1] * activeBrush.a)) + (activeBrush.r * activeBrush.a)}, ${(rgb[2] - (rgb[2] * activeBrush.a)) + (activeBrush.g * activeBrush.a)}, ${(rgb[3] - (rgb[3] * activeBrush.a)) + (activeBrush.b * activeBrush.a) });`);
+
+  //  console.log(event.target.getAttribute('style'));
 }
 
 function changeBrush(r,g,b,a){
-    activeBrush = `rgba(${r}, ${g}, ${b}, ${a})`;
+    activeBrush.r = r;
+    activeBrush.g = g;
+    activeBrush.b = b;
+    activeBrush.a = a;
 }
 
 //initializing event listeners
@@ -144,7 +143,7 @@ function changeOpacity(event) {
     if (textOpc.value > 0 && textOpc.value <= 1) {
         textOpc.blur();
         opcSlider.value = textOpc.value;
-        changeBrush(r,g,b, opcSlider.value);
+        changeBrush(activeBrush.r,activeBrush.g,activeBrush.b, opcSlider.value);
     }
 
     else {
@@ -160,14 +159,24 @@ function toggleButton(event) {
     document.getElementById('eraser-indicator').style.display = 'none';
     switch (event.target.getAttribute('id')) {
         case 'color':
-            document.getElementById('color-indicator').style.display = 'block';
+            document.getElementById('color-indicator').style.display = 'block'; activeBrush.r = colorBrush.r;
+            activeBrush.g = colorBrush.g;
+            activeBrush.b = colorBrush.b;
+            activeBrush.a = colorBrush.a;
+            opcSlider.value = colorBrush.a;
+            textOpc.value = colorBrush.a;
             return;
         case 'rainbow':
             document.getElementById('rainbow-indicator').style.display = 'block';
             return;
         case 'eraser':
             document.getElementById('eraser-indicator').style.display = 'block';
-            activeBrush = ERASER_BRUSH;
+            activeBrush.r = 255;
+            activeBrush.g = 255;
+            activeBrush.b = 255; 
+            activeBrush.a = 1;
+            opcSlider.value = 1;
+            textOpc.value = 1;           
             return;
     }
 
@@ -184,7 +193,7 @@ function eventListenersFunction(event) {
     document.getElementById('opacity-form').addEventListener('submit', changeOpacity);
     opcSlider.addEventListener('change', () => {
         textOpc.value = opcSlider.value;
-        changeBrush(r,g,b,opcSlider.value);
+        changeBrush(activeBrush.r,activeBrush.g,activeBrush.b,opcSlider.value);
     })
 
     pixelSlider.addEventListener('change', (event) => {
@@ -204,3 +213,15 @@ function eventListenersFunction(event) {
 window.matchMedia('(min-width: 768px)').addEventListener('change', () => {
     updateGridsSize(grids);
 });
+
+Math.Clamp = (value, min, max) => {
+    if(value < min){
+        return min;
+    }
+
+    else if(value > max){
+        return max;
+    }
+
+    return value;
+}
